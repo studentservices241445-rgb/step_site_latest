bce89fddf298b0889c6c377245216ecc004fd217
studentservices241445-rgb <studentservices241445@gmail.com>
2026-02-07 16:09:10 -0800

    Add files via upload

---
+++ assets/js/site-data.js
@@ -0,0 +1,12 @@
+const SITE_DATA = {
+  programName: 'اختبار STEP 2026 – أكاديمية عايد الرسمية',
+  bank: {
+    bankName: 'بنك الإنماء',
+    accountNumber: '68206067557000',
+    iban: 'SA4905000068206067557000',
+    beneficiary: 'مؤسسة كريتيفا جلوبال لتقنية المعلومات'
+  },
+  telegram: '@Ayed_Academy_2026',
+  courseLink: 'https://ayedacademy2026.github.io/ayed-step-academy2026/',
+  starsLink: 'https://t.me/+BKZFAaIFbe4zOTk0'
+};
